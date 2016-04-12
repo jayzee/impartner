682045f0f074df47089927f6d7fecf3608c31026
jayzee <joshzizmor@gmail.com>
2016-04-12 17:35:32 -0400

    stuff done

--- app/assets/javascripts/resource.js
+++ app/assets/javascripts/resource.js
@@ -1,12 +1,11 @@
 $(function(
   $('btn btn-primary').on('click', function(){
-    var resourceId = $(this).id
-    binding.pry
-    $.ajax({
-      method: 'POST',
-      url: '/resources/'+ resourceId+'/complete'
-    })
-
+      var resourceId = $(this).id
+      binding.pry
+      $.ajax({
+        method: 'POST',
+        url: '/resources/' + resourceId + '/complete'
+      })
 
   })
 
