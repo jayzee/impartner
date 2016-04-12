74b25a40345be9cdd0974362af626e8eb27049d7
HarrisonLavin <harrison.lavin@gmail.com>
2016-04-12 09:52:33 -0400

    adds authorized_to_edit to lessons, resources, and tracks

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -14,4 +14,16 @@
 #
 
 class ResourcesController < ApplicationController
+
+  before_action :authorized_to_interact, only: [:edit] 
+
+  private
+
+  def authorized_to_edit
+    @resource=Resource.find(params[:id])
+    @teacher= Teacher.find(@resource.lesson.track.teacher_id)
+    if session[:user_id] != @teacher.user_id
+        redirect_to root_path, notice: "You must be a Track's teacher in order to edit its contents" 
+    end
+  end
 end
