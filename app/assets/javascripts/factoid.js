613781d5dd3a90389c295af63ffb90bf53e6cf37
HarrisonLavin <harrison.lavin@gmail.com>
2016-04-14 16:41:26 -0400

    .

--- app/assets/javascripts/factoid.js
+++ app/assets/javascripts/factoid.js
@@ -1,14 +1,14 @@
-var listing;
-var fact;
+// var listing;
+// var fact;
 
-$(function(){
-  $.getJSON("https://www.reddit.com/r/funfacts/random.json", function(data){
-    listing = data[0].data.children[0].data;
-    fact = listing.data.title
-    debugger; 
-  })
+// $(function(){
+//   $.getJSON("https://www.reddit.com/r/funfacts/random.json", function(data){
+//     listing = data[0].data.children[0].data;
+//     fact = listing.data.title
+//     debugger; 
+//   })
 
-  $(document).ajaxSuccess(function(){
-    $()
-  })
-})
+//   $(document).ajaxSuccess(function(){
+//     $()
+//   })
+// })
