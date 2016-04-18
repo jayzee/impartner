2ff16695057f7f4956b59716d8a9b19d03ef8562
HarrisonLavin <harrison.lavin@gmail.com>
2016-04-18 13:46:16 -0400

    various housekeeping

--- app/assets/javascripts/factoid.js
+++ app/assets/javascripts/factoid.js
@@ -1,18 +1,3 @@
-// var listing;
-// var fact;
-
-// $(function(){
-//   $.getJSON("https://www.reddit.com/r/funfacts/random.json", function(data){
-//     listing = data[0].data.children[0].data;
-//     fact = listing.data.title
-//     debugger; 
-//   })
-
-//   $(document).ajaxSuccess(function(){
-//     $()
-//   })
-// })
-
 $(function(){
   $('.random-fact button').on("click", function(){
     $.ajax({
@@ -24,7 +9,6 @@ $(function(){
           $('.random-fact button').append(url)
         })
     })
-    // $('.random-fact button').replaceWith("<h3>Loading Fact...</h3>")
     $('.random-fact button').prop( "disabled", true );
     $('.random-fact button').html("<img width= '50' src='http://sampson.washcoll.edu/~ksugalski2/final%20project/images/squid3.gif'>")
   })
